85f675b03a58f95f29324b7baeaa08d0889a11ef
byrdsd <seanbyrd@yahoo.com>
2021-02-11 23:54:50 -0600

    Add basic accessibility

--- art_poc_react/src/components/Flashcard.jsx
+++ art_poc_react/src/components/Flashcard.jsx
@@ -1,16 +1,23 @@
+import React from 'react';
 import './Flashcard.css';
 
 const Flashcard = ({ front, front_image, back, back_image }) => {
+  const [flipped, setFlipped] = React.useState(false);
+
+  const flipCard = () => {
+    setFlipped(!flipped)
+  }
+
   return (
-    <div className="Flashcard" onClick={(e) => e.target.closest('.Flashcard').classList.toggle('Flashcard--flipped')}>
-      <div className="Flashcard-front">
+    <div className={`Flashcard${flipped ? " Flashcard--flipped" : ""}`} onClick={flipCard} onKeyPress={(event) => event.key === "Enter" || event.code === "Space" ? flipCard() : {}}>
+      <div className="Flashcard-front" aria-hidden={flipped} tabIndex={flipped ? -1 : 0}>
         {
           front_image ?
         <img className="Flashcard-image" alt="  " src={front}></img> :
             front
         }
       </div>
-      <div className="Flashcard-back">
+      <div className="Flashcard-back" aria-hidden={!flipped} tabIndex={flipped ? 0 : -1}>
         {
           back_image ?
           <img className="Flashcard-image" alt="" src={back}></img> :
